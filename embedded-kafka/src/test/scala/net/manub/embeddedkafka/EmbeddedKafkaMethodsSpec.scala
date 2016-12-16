embeddedkafka/embedded-kafka

commit a90674307eb15206e4f75619beae067124d989d6

Using 0.9.0 consumer in code and tests - related to #17.

src/main/scala/net/manub/embeddedkafka/EmbeddedKafka.scala
@@ -5,22 +5,22 @@ import java.util.Properties
 import java.util.concurrent.Executors
 
 import kafka.admin.AdminUtils
-import kafka.consumer.{Consumer, ConsumerConfig, Whitelist}
-import kafka.serializer.{Decoder, StringDecoder}
 import kafka.server.{KafkaConfig, KafkaServer}
 import kafka.utils.ZkUtils
+import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
-import org.apache.kafka.common.serialization.{Serializer, StringSerializer}
+import org.apache.kafka.common.KafkaException
+import org.apache.kafka.common.serialization.{Deserializer, Serializer, StringDeserializer, StringSerializer}
 import org.apache.zookeeper.server.{ServerCnxnFactory, ZooKeeperServer}
 import org.scalatest.Suite
 
+import scala.collection.JavaConversions
 import scala.collection.JavaConversions.mapAsJavaMap
-import scala.concurrent._
 import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, TimeoutException}
 import scala.language.{higherKinds, postfixOps}
 import scala.reflect.io.Directory
 import scala.util.Try
-import scala.util.control.NonFatal
 
 trait EmbeddedKafka extends EmbeddedKafkaSupport {
   this: Suite =>
@@ -77,14 +77,14 @@ sealed trait EmbeddedKafkaSupport {
   val executorService = Executors.newFixedThreadPool(2)
   implicit val executionContext = ExecutionContext.fromExecutorService(executorService)
 
-  val zkSessionTimeoutMs  = 10000
+  val zkSessionTimeoutMs = 10000
   val zkConnectionTimeoutMs = 10000
   val zkSecurityEnabled = false
 
   /**
     * Starts a ZooKeeper instance and a Kafka broker, then executes the body passed as a parameter.
     *
-    * @param body the function to execute
+    * @param body   the function to execute
     * @param config an implicit [[EmbeddedKafkaConfig]]
     */
   def withRunningKafka(body: => Unit)(implicit config: EmbeddedKafkaConfig) = {
@@ -104,9 +104,9 @@ sealed trait EmbeddedKafkaSupport {
     * Publishes synchronously a message of type [[String]] to the running Kafka broker.
     *
     * @see [[EmbeddedKafka#publishToKafka]]
-    * @param topic the topic to which publish the message (it will be auto-created)
+    * @param topic   the topic to which publish the message (it will be auto-created)
     * @param message the [[String]] message to publish
-    * @param config an implicit [[EmbeddedKafkaConfig]]
+    * @param config  an implicit [[EmbeddedKafkaConfig]]
     * @throws KafkaUnavailableException if unable to connect to Kafka
     */
   def publishStringMessageToKafka(topic: String, message: String)(implicit config: EmbeddedKafkaConfig): Unit =
@@ -115,9 +115,9 @@ sealed trait EmbeddedKafkaSupport {
   /**
     * Publishes synchronously a message to the running Kafka broker.
     *
-    * @param topic the topic to which publish the message (it will be auto-created)
-    * @param message the message of type [[T]] to publish
-    * @param config an implicit [[EmbeddedKafkaConfig]]
+    * @param topic      the topic to which publish the message (it will be auto-created)
+    * @param message    the message of type [[T]] to publish
+    * @param config     an implicit [[EmbeddedKafkaConfig]]
     * @param serializer an implicit [[Serializer]] for the type [[T]]
     * @throws KafkaUnavailableException if unable to connect to Kafka
     */
@@ -144,48 +144,46 @@ sealed trait EmbeddedKafkaSupport {
   }
 
   def consumeFirstStringMessageFrom(topic: String)(implicit config: EmbeddedKafkaConfig): String =
-    consumeFirstMessageFrom(topic)(config, new StringDecoder())
+    consumeFirstMessageFrom(topic)(config, new StringDeserializer())
 
 
   /**
     * Consumes the first message available in a given topic, deserializing it as a String.
     *
-    * @param topic the topic to consume a message from
-    * @param config an implicit [[EmbeddedKafkaConfig]]
-    * @param decoder an implicit [[Decoder]] for the type [[T]]
+    * @param topic        the topic to consume a message from
+    * @param config       an implicit [[EmbeddedKafkaConfig]]
+    * @param deserializer an implicit [[org.apache.kafka.common.serialization.Deserializer]] for the type [[T]]
     * @return the first message consumed from the given topic, with a type [[T]]
     * @throws TimeoutException if unable to consume a message within 5 seconds
     * @throws KafkaUnavailableException if unable to connect to Kafka
     */
   @throws(classOf[TimeoutException])
   @throws(classOf[KafkaUnavailableException])
-  def consumeFirstMessageFrom[T](topic: String)(implicit config: EmbeddedKafkaConfig, decoder: Decoder[T]): T = {
+  def consumeFirstMessageFrom[T](topic: String)(implicit config: EmbeddedKafkaConfig, deserializer: Deserializer[T]): T = {
+
+    import scala.collection.JavaConversions._
+
     val props = new Properties()
     props.put("group.id", s"embedded-kafka-spec")
-    props.put("zookeeper.connect", s"localhost:${config.zooKeeperPort}")
-    props.put("auto.offset.reset", "smallest")
-    props.put("zookeeper.connection.timeout.ms", "6000")
+    props.put("bootstrap.servers", s"localhost:${config.kafkaPort}")
+    props.put("auto.offset.reset", "earliest")
 
-    val consumer =
-      try Consumer.create(new ConsumerConfig(props))
-      catch {
-        case NonFatal(e) =>
-          throw new KafkaUnavailableException(e)
+    val consumer = new KafkaConsumer[String, T](props, new StringDeserializer, deserializer)
+
+    val message = Try {
+      consumer.subscribe(List(topic))
+      consumer.partitionsFor(topic) // as poll doesn't honour the timeout, forcing the consumer to fail here.
+      val records = consumer.poll(5000)
+      if (records.isEmpty) {
+        throw new TimeoutException("Unable to retrieve a message from Kafka in 5000ms")
       }
-
-    val messageStreams =
-      consumer.createMessageStreamsByFilter(Whitelist(topic), keyDecoder = new StringDecoder, valueDecoder = decoder)
-
-    val messageFuture = Future {
-      messageStreams.headOption
-        .getOrElse(throw new KafkaSpecException("Unable to find a message stream")).iterator().next().message()
+      records.iterator().next().value()
     }
 
-    try {
-      Await.result(messageFuture, 5 seconds)
-    } finally {
-      consumer.shutdown()
-    }
+    consumer.close()
+    message.recover {
+      case ex: KafkaException => throw new KafkaUnavailableException(ex)
+    }.get
   }
 
   object aKafkaProducer {

src/main/scala/net/manub/embeddedkafka/exceptions.scala
@@ -1,5 +1,8 @@
 package net.manub.embeddedkafka
 
-class KafkaUnavailableException(cause: Throwable) extends RuntimeException(cause)
+class KafkaUnavailableException(msg: String, cause: Throwable) extends RuntimeException(msg, cause) {
+  def this(msg: String) = this(msg, null)
+  def this(cause: Throwable) = this(null, cause)
+}
 
 class KafkaSpecException(msg: String) extends RuntimeException(msg)

src/test/scala/net/manub/embeddedkafka/EmbeddedKafkaSpec.scala
@@ -4,17 +4,14 @@ import java.util.Properties
 import java.util.concurrent.TimeoutException
 
 import kafka.admin.AdminUtils
-import kafka.consumer.{Consumer, ConsumerConfig, Whitelist}
-import kafka.serializer.StringDecoder
 import kafka.utils.ZkUtils
+import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
-import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
+import org.apache.kafka.common.serialization.{ByteArraySerializer, StringDeserializer, StringSerializer}
 import org.scalatest.exceptions.TestFailedException
 import org.scalatest.time.{Milliseconds, Seconds, Span}
 
 import scala.collection.JavaConversions._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 import scala.language.postfixOps
 
 class EmbeddedKafkaSpec extends EmbeddedKafkaSpecSupport with EmbeddedKafka {
@@ -85,27 +82,17 @@ class EmbeddedKafkaSpec extends EmbeddedKafkaSpecSupport with EmbeddedKafka {
 
         publishStringMessageToKafka(topic, message)
 
-        val consumer = Consumer.create(consumerConfigForEmbeddedKafka)
+        val consumer = new KafkaConsumer[String, String](consumerProps, new StringDeserializer, new StringDeserializer)
+        consumer.subscribe(List("test_topic"))
 
-        val filter = new Whitelist("test_topic")
-        val stringDecoder = new StringDecoder
+        val records = consumer.poll(ConsumerPollTimeout)
 
-        val messageStreams = consumer.createMessageStreamsByFilter(filter, 1, stringDecoder, stringDecoder)
+        records.iterator().hasNext shouldBe true
+        val record = records.iterator().next()
 
-        val eventualMessage = Future {
-          messageStreams
-            .headOption
-            .getOrElse(throw new RuntimeException("Unable to retrieve message streams"))
-            .iterator()
-            .next()
-            .message()
-        }
+        record.value() shouldBe message
 
-        whenReady(eventualMessage) { msg =>
-          msg shouldBe message
-        }
-
-        consumer.shutdown()
+        consumer.close()
       }
     }
 
@@ -184,7 +171,7 @@ class EmbeddedKafkaSpec extends EmbeddedKafkaSpecSupport with EmbeddedKafka {
 
         val message = TestAvroClass("name")
         val topic = "test_topic"
-        implicit val testAvroClassDecoder = specificAvroDecoder[TestAvroClass](TestAvroClass.SCHEMA$)
+        implicit val testAvroClassDecoder = specificAvroDeserializer[TestAvroClass](TestAvroClass.SCHEMA$)
 
         val producer = new KafkaProducer[String, TestAvroClass](Map(
           ProducerConfig.BOOTSTRAP_SERVERS_CONFIG -> s"localhost:6001"
@@ -243,12 +230,13 @@ class EmbeddedKafkaSpec extends EmbeddedKafkaSpecSupport with EmbeddedKafka {
     }
   }
 
-  lazy val consumerConfigForEmbeddedKafka: ConsumerConfig = {
+  lazy val consumerProps: Properties = {
     val props = new Properties()
     props.put("group.id", "test")
-    props.put("zookeeper.connect", "localhost:6000")
-    props.put("auto.offset.reset", "smallest")
-
-    new ConsumerConfig(props)
+    props.put("bootstrap.servers", "localhost:6001")
+    props.put("auto.offset.reset", "earliest")
+    props
   }
+
+  val ConsumerPollTimeout = 3000
 }

manub/scalatest-embedded-kafka

commit 96251d71a0e38de9ca6b187ea4649a5e8489a687

Allow consumeFirstStringMessageFrom to be called multiple times
In v0.5.0 you were able to call consumeFirstStringMessageFrom multiple
times, with each invocation causing the next message in a topic to be
consumed. In v0.10.0 this was no longer the case: the first invocation
works fine, but subsequent invocations would result in exceptions as the
first invocation would cause *all* messages to be committed even though
only the first one is returned, leaving no messages remaining to be
consumed by subsequent calls. This regression was likely introduced by
commit a90674307eb15206e4f75619beae067124d989d6.

This commit fixes this by disabling auto-commit by default and simply
committing only the message that consumeFirstStringMessageFrom returns.

embedded-kafka/src/main/scala/net/manub/embeddedkafka/EmbeddedKafka.scala
@@ -7,9 +7,9 @@ import java.util.concurrent.Executors
 import kafka.admin.AdminUtils
 import kafka.server.{KafkaConfig, KafkaServer}
 import kafka.utils.ZkUtils
-import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.consumer.{KafkaConsumer, OffsetAndMetadata}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
-import org.apache.kafka.common.KafkaException
+import org.apache.kafka.common.{KafkaException, TopicPartition}
 import org.apache.kafka.common.serialization.{Deserializer, Serializer, StringDeserializer, StringSerializer}
 import org.apache.zookeeper.server.{ServerCnxnFactory, ZooKeeperServer}
 import org.scalatest.Suite
@@ -217,6 +217,8 @@ sealed trait EmbeddedKafkaSupport {
   /**
     * Consumes the first message available in a given topic, deserializing it as a String.
     *
+    * Only the messsage that is returned is committed if config.autoCommit is false. If config.autoCommit is true then all messages that were polled will be committed.
+    *
     * @param topic        the topic to consume a message from
     * @param config       an implicit [[EmbeddedKafkaConfig]]
     * @param deserializer an implicit [[org.apache.kafka.common.serialization.Deserializer]] for the type [[T]]
@@ -236,6 +238,7 @@ sealed trait EmbeddedKafkaSupport {
     props.put("group.id", s"embedded-kafka-spec")
     props.put("bootstrap.servers", s"localhost:${config.kafkaPort}")
     props.put("auto.offset.reset", "earliest")
+    props.put("enable.auto.commit", s"${config.autoCommit}")
 
     val consumer =
       new KafkaConsumer[String, T](props, new StringDeserializer, deserializer)
@@ -248,7 +251,14 @@ sealed trait EmbeddedKafkaSupport {
         throw new TimeoutException(
           "Unable to retrieve a message from Kafka in 5000ms")
       }
-      records.iterator().next().value()
+
+      val record = records.iterator().next()
+
+      val tp = new TopicPartition(record.topic(), record.partition())
+      val om = new OffsetAndMetadata(record.offset() + 1)
+      consumer.commitSync(Map(tp -> om))
+
+      record.value()
     }
 
     consumer.close()

embedded-kafka/src/main/scala/net/manub/embeddedkafka/EmbeddedKafkaConfig.scala
@@ -2,7 +2,8 @@ package net.manub.embeddedkafka
 
 case class EmbeddedKafkaConfig(kafkaPort: Int = 6001,
                                zooKeeperPort: Int = 6000,
-                               customBrokerProperties: Map[String, String] = Map.empty)
+                               customBrokerProperties: Map[String, String] = Map.empty,
+                               autoCommit: Boolean = false)
 
 object EmbeddedKafkaConfig {
   implicit val defaultConfig = EmbeddedKafkaConfig()

embedded-kafka/src/test/scala/net/manub/embeddedkafka/EmbeddedKafkaMethodsSpec.scala
@@ -28,12 +28,12 @@ class EmbeddedKafkaMethodsSpec extends EmbeddedKafkaSpecSupport with EmbeddedKaf
     "publish synchronously a String message to Kafka" in {
       implicit val serializer = new StringSerializer()
       val message = "hello world!"
-      val topic = "test_topic"
+      val topic = "publish_test_topic"
 
       publishToKafka(topic, message)
 
       val consumer = new KafkaConsumer[String, String](consumerProps, new StringDeserializer, new StringDeserializer)
-      consumer.subscribe(List("test_topic"))
+      consumer.subscribe(List(topic))
 
       val records = consumer.poll(ConsumerPollTimeout)
 
@@ -50,12 +50,12 @@ class EmbeddedKafkaMethodsSpec extends EmbeddedKafkaSpecSupport with EmbeddedKaf
       implicit val serializer = new StringSerializer()
       val key = "key"
       val message = "hello world!"
-      val topic = "test_topic"
+      val topic = "publish_test_topic"
 
       publishToKafka(topic, key, message)
 
       val consumer = new KafkaConsumer[String, String](consumerProps, new StringDeserializer, new StringDeserializer)
-      consumer.subscribe(List("test_topic"))
+      consumer.subscribe(List(topic))
 
       val records = consumer.poll(ConsumerPollTimeout)
 
@@ -111,7 +111,7 @@ class EmbeddedKafkaMethodsSpec extends EmbeddedKafkaSpecSupport with EmbeddedKaf
   "the consumeFirstStringMessageFrom method" should {
     "return a message published to a topic" in {
       val message = "hello world!"
-      val topic = "test_topic"
+      val topic = "consume_test_topic"
 
       val producer = new KafkaProducer[String, String](Map(
         ProducerConfig.BOOTSTRAP_SERVERS_CONFIG -> s"localhost:6001",
@@ -126,9 +126,34 @@ class EmbeddedKafkaMethodsSpec extends EmbeddedKafkaSpecSupport with EmbeddedKaf
       producer.close()
     }
 
+    "consume only a single message when multiple messages have been published to a topic" in {
+      val messages = Set("message 1", "message 2", "message 3")
+      val topic = "consume_test_topic"
+
+      val producer = new KafkaProducer[String, String](Map(
+        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG -> s"localhost:6001",
+        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG -> classOf[StringSerializer].getName,
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG -> classOf[StringSerializer].getName
+      ))
+
+      messages.foreach { message =>
+        producer.send(new ProducerRecord[String, String](topic, message))
+      }
+
+      producer.flush()
+
+      val consumedMessages = for (i <- 1 to messages.size) yield {
+        consumeFirstStringMessageFrom(topic)
+      }
+
+      consumedMessages.toSet shouldEqual messages
+
+      producer.close()
+    }
+
     "return a message published to a topic with implicit decoder" in {
       val message = "hello world!"
-      val topic = "test_topic"
+      val topic = "consume_test_topic"
 
       val producer = new KafkaProducer[String, String](Map(
         ProducerConfig.BOOTSTRAP_SERVERS_CONFIG -> s"localhost:6001",
@@ -149,7 +174,7 @@ class EmbeddedKafkaMethodsSpec extends EmbeddedKafkaSpecSupport with EmbeddedKaf
       import avro._
 
       val message = TestAvroClass("name")
-      val topic = "test_topic"
+      val topic = "consume_test_topic"
       implicit val testAvroClassDecoder = specificAvroDeserializer[TestAvroClass](TestAvroClass.SCHEMA$)
 
       val producer = new KafkaProducer[String, TestAvroClass](Map(