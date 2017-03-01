c22209679bd9328d1ca75352b79fc97fcc97ddf2
marcin <marcin.kacperek@performgroup.com>
2017-03-01 14:29:00 +0100

    Clean code fixes

--- embedded-kafka/src/test/scala/net/manub/embeddedkafka/EmbeddedKafkaCustomConfigSpec.scala
+++ embedded-kafka/src/test/scala/net/manub/embeddedkafka/EmbeddedKafkaCustomConfigSpec.scala
@@ -3,24 +3,24 @@ package net.manub.embeddedkafka
 import scala.language.postfixOps
 
 class EmbeddedKafkaCustomConfigSpec extends EmbeddedKafkaSpecSupport with EmbeddedKafka {
-  val TWO_MEGABYTES = 2097152
-  val THREE_MEGABYTES = 3145728
+  val TwoMegabytes = 2097152
+  val ThreeMegabytes = 3145728
 
   "the custom config" should {
-    "should allow pass additional producer parameters" in {
+    "allow pass additional producer parameters" in {
       val customBrokerConfig = Map(
-        "replica.fetch.max.bytes" -> s"$THREE_MEGABYTES",
-        "message.max.bytes" -> s"$THREE_MEGABYTES")
+        "replica.fetch.max.bytes" -> s"$ThreeMegabytes",
+        "message.max.bytes" -> s"$ThreeMegabytes")
 
-      val customProducerConfig = Map("max.request.size" -> s"$THREE_MEGABYTES")
-      val customConsumerConfig = Map("max.partition.fetch.bytes" -> s"$THREE_MEGABYTES")
+      val customProducerConfig = Map("max.request.size" -> s"$ThreeMegabytes")
+      val customConsumerConfig = Map("max.partition.fetch.bytes" -> s"$ThreeMegabytes")
 
       implicit val customKafkaConfig = EmbeddedKafkaConfig(
         customBrokerProperties = customBrokerConfig,
         customProducerProperties = customProducerConfig,
         customConsumerProperties = customConsumerConfig)
 
-      val bigMessage = generateMessageOfLength(TWO_MEGABYTES)
+      val bigMessage = generateMessageOfLength(TwoMegabytes)
       val topic = "big-message-topic"
 
       withRunningKafka {
