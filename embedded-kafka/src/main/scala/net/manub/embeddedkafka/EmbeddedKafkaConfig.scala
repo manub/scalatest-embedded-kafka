264eaf24b379bd72027749db1b26bd179f68967e
Maxence Cramet <maxence.cramet@tritondigital.com>
2017-09-15 15:04:31 -0400

    Make EmbeddedKafkaConfig a trait with a basic implementation

--- embedded-kafka/src/main/scala/net/manub/embeddedkafka/EmbeddedKafkaConfig.scala
+++ embedded-kafka/src/main/scala/net/manub/embeddedkafka/EmbeddedKafkaConfig.scala
@@ -1,11 +1,30 @@
 package net.manub.embeddedkafka
 
-case class EmbeddedKafkaConfig(kafkaPort: Int = 6001,
-                               zooKeeperPort: Int = 6000,
-                               customBrokerProperties: Map[String, String] = Map.empty,
-                               customProducerProperties: Map[String, String] = Map.empty,
-                               customConsumerProperties: Map[String, String] = Map.empty)
+trait EmbeddedKafkaConfig {
+  val kafkaPort: Int
+  val zooKeeperPort: Int
+  val customBrokerProperties: Map[String, String]
+  val customProducerProperties: Map[String, String]
+  val customConsumerProperties: Map[String, String]
+}
+
+case class EmbeddedKafkaConfigImpl(
+                                    kafkaPort: Int,
+                                    zooKeeperPort: Int,
+                                    customBrokerProperties: Map[String, String],
+                                    customProducerProperties: Map[String, String],
+                                    customConsumerProperties: Map[String, String]
+                                  ) extends EmbeddedKafkaConfig
 
 object EmbeddedKafkaConfig {
-  implicit val defaultConfig = EmbeddedKafkaConfig()
+  implicit val defaultConfig: EmbeddedKafkaConfig = apply()
+
+  def apply(
+             kafkaPort: Int = 6001,
+             zooKeeperPort: Int = 6000,
+             customBrokerProperties: Map[String, String] = Map.empty,
+             customProducerProperties: Map[String, String] = Map.empty,
+             customConsumerProperties: Map[String, String] = Map.empty
+           ) : EmbeddedKafkaConfig =
+    EmbeddedKafkaConfigImpl(kafkaPort, zooKeeperPort, customBrokerProperties, customProducerProperties, customConsumerProperties)
 }
